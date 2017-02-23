bb98b95f022a20900aba1312e15f89d3a84dce26
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-23 16:49:53 +0200

    Update formating

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -2,11 +2,11 @@ import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
 export function loadCourses() {
-    return function(dispatch) {
+    return function (dispatch) {
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
-        }).catch(error=>{
-            throw(error);
+        }).catch(error => {
+            throw (error);
         });
     };
 }
