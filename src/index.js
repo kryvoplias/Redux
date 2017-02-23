773287a37c92b1c9b6f8b266f0a69804092c9a06
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-23 14:14:20 +0200

    Loading list of courses from API via Thunk.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import configureStore from './store/configureStore';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
