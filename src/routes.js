c4418353dbd5bd8c084a0c4726a8e4ab151b63ef
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-23 16:35:01 +0200

    Updating routes to be able to view and edit course

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import Layout from './components/Layout';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import UpdateCoursePage from './components/course/UpdateCoursePage';
 
 export default (
     <Route path="/" component={Layout}>
         <IndexRoute component={HomePage} />
-        <Route path="about" component={AboutPage} />
         <Route path="courses" component={CoursesPage} />
+        <Route path="course" component={UpdateCoursePage} />
+        <Route path="course/:id" component={UpdateCoursePage} />
+        <Route path="about" component={AboutPage} />
     </Route>
 );
