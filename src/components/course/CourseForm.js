5992343ea773954468aa4ffe9af01ce0e96b0466
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-23 16:50:33 +0200

    Introduce view/update course page

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -5,7 +5,7 @@ import SelectInput from '../common/SelectInput';
 const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
     return (
         <form>
-            <h1></h1>
+            <h1>Update Course</h1>
             <TextInput
                 name="title"
                 label="Title"
--- src/components/course/UpdateCoursePage.js
+++ src/components/course/UpdateCoursePage.js
@@ -2,26 +2,37 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class UpdateCoursePage extends Component {
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        };
     }
 
     render() {
         return (
-            <h1>Update Course</h1>
+            <CourseForm
+                course={this.state.course}
+                errors={this.state.errors}
+                allAuthors={[]} />
         );
     }
 }
 
 UpdateCoursePage.propTypes = {
+    course: PropTypes.object.isRequired
+    //errors: PropTypes.arrayOf(PropTypes.string)
 };
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        state: state
-    };
+    let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+
+    return { course };
 };
 
 const mapDispatchToProps = (dispatch) => ({
