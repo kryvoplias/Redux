fb48917a5a318fd10ac4214c6601d2db25b39985
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-24 09:21:12 +0200

    Add actions to update/create courses

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,18 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
+export function updateCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function loadCoursesSuccess(courses) {
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
 export function loadCourses() {
     return function (dispatch) {
         return courseApi.getAllCourses().then(courses => {
@@ -11,6 +23,13 @@ export function loadCourses() {
     };
 }
 
-export function loadCoursesSuccess(courses) {
-    return { type: types.LOAD_COURSES_SUCCESS, courses };
+export function saveCourse(course) {
+    return function (dispatch) {
+        return courseApi.saveCourse(course).then(course => {
+            course.id ? dispatch(updateCourseSuccess(course)) :
+                dispatch(createCourseSuccess(course));
+        }).catch(error => {
+            throw (error);
+        });
+    };
 }
